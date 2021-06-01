0de3d8a5d8b5543c5226a47036576178350d65a8
pjamsa <Paul_Jamsa@msn.com>
2021-06-01 09:54:24 -0700

    Initial Build

---
+++ WhileDo/WhileDo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace WhileDo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            while (number < 10)
+            {
+                Console.WriteLine(number);
+                number++;
+            }
+
+            int num = 10;
+            do
+            {
+                Console.WriteLine(num);
+                num++;
+            }
+            while (num < 20);
+                
+        }
+    }
+}
