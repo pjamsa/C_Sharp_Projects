e714e7457ccfa1fe0cf6a63bfac126de5cedcc85
pjamsa <Paul_Jamsa@msn.com>
2021-06-01 12:17:39 -0700

    Initial Build

---
+++ StringAssignment/StringAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = "Paul";
+            string middle = "William";
+            string last = "Jamsa";
+
+            Console.WriteLine(first + " "  + middle + " " + last);
+            string up = first.ToUpper();
+            Console.WriteLine(up);
+
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            sb.Append("I am the most interesting man in the a romm.");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
