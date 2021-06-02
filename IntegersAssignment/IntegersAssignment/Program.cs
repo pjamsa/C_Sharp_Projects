a2c1aab9cec26dd3cb057e447ff23bd9c3b30b26
pjamsa <Paul_Jamsa@msn.com>
2021-06-02 16:57:10 -0700

    Added comments

--- IntegersAssignment/IntegersAssignment/Program.cs
+++ IntegersAssignment/IntegersAssignment/Program.cs
@@ -8,8 +8,10 @@ namespace IntegersAssignment
     {
         static void Main(string[] args)
         {
+            //try block handles errors 
             try
             {
+                // creates list and asks for user input 
                 List<int> ages = new List<int> { 25, 24, 23, 22, 40, 55, 20, 49, 19, 21 };
                 Console.WriteLine("Please choose a number.");
                 int userNum = Convert.ToInt32(Console.ReadLine());
@@ -18,6 +20,7 @@ namespace IntegersAssignment
                     Console.WriteLine(age / userNum);
                 }
             }
+            // these catch blocks handle any erors the user might encounter from user error
             catch (FormatException ex)
             {
                 Console.WriteLine("Please enter a whole number.");
@@ -31,6 +34,7 @@ namespace IntegersAssignment
             {
                 Console.WriteLine(ex);
             }
+            // this block let's users know that they have left the try/catch block
             finally
             {
                 Console.WriteLine("Program has left the try/catch block and continued with program execution");
