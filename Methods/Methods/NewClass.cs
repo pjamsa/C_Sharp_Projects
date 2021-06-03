c4e942753105f71bdddbb99ec9656ea7d84fe34e
pjamsa <Paul_Jamsa@msn.com>
2021-06-03 11:31:18 -0700

    Initial Build

---
+++ Methods/Methods/NewClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    // New class name
+    public class NewClass
+    {
+        //first method adds passed parameter
+        public int Add(int x)
+        {
+            int result = x + 25;
+            return result;
+        }   
+
+        // second method subtracts passed parameter
+        public int Subtract(int y)
+        {
+            int result = y - 5;
+            return result;
+        }
+
+        // third method Multiples passed parameter
+        public int Multiply(int z)
+        {
+            int result = z * 2;
+            return result;
+        }
+
+
+
+
+    }
+}
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creates object from created NewClass
+            NewClass myObj = new NewClass();
+            
+            //Gathers 
+            Console.WriteLine("Please enter a whole number");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            // Calls passes paramater to NewClass methods and then calls method
+            int answer1 = myObj.Add(userNum);
+            Console.WriteLine(answer1);
+
+            int answer2 = myObj.Subtract(userNum);
+            Console.WriteLine(answer2);
+
+            int answer3 = myObj.Multiply(userNum);
+            Console.WriteLine(answer3);
+        }
+    }
+}
