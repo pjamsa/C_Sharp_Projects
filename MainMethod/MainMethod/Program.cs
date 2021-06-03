654e584d26c69ba8506e45298d58b3a841149b8e
pjamsa <Paul_Jamsa@msn.com>
2021-06-03 14:11:03 -0700

    Initial Build

---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create object 
+            Thing myObj = new Thing();
+
+            // passing values to methods and then printing the response to the application
+            Console.WriteLine(myObj.Add(6));
+            Console.WriteLine(myObj.Add(3.2));
+            Console.WriteLine(myObj.Add("100"));
+
+        }
+    }
+}
---
+++ MainMethod/MainMethod/Thing.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMethod
+{
+    class Thing
+    {
+        //Method Overloading Class Methods
+        public int Add(int x)
+        {
+            int result = x + 1000;
+            return result;
+        }
+        
+        public double Add(double x)
+        {
+            double result = x + 300;
+            return result;
+        }
+        
+        public string Add(string x)
+        {
+            //have to change string value to an integer
+            int change = Convert.ToInt32(x);
+            int result = change + 100;
+            // compiler cannot implicitly change int to string
+            //that's why i had to add .ToString(); 
+            return result.ToString();
+        }
+
+    }
+}
