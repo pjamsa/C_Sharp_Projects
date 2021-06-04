965e75b6566a4912a7b78aa90fcb9715cd010b99
pjamsa <Paul_Jamsa@msn.com>
2021-06-04 10:18:48 -0700

    Initial Build

---
+++ Methods2/Methods2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace Methods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // new object created
+            Thing myObj = new Thing();
+
+            //Gets input from user
+            Console.WriteLine("Please insert a whole number.");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number or not. The choice is ours.");
+
+            int y;
+            int answer;  
+            
+            // out keyword passes argument to method
+            if(int.TryParse(Console.ReadLine(), out y))
+            {
+                answer = myObj.Add(x, y);
+            }
+            else
+            {
+                answer = myObj.Add(x);
+            }
+            Console.WriteLine(answer);
+            
+            
+            //int y = Convert.ToInt32(Console.ReadLine());
+            //int answer = myObj.Add(x, y);
+            //Console.WriteLine("\n" + answer1);
+
+
+
+        }
+    }
+}
---
+++ Methods2/Methods2/Thing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Methods2
+{
+    class Thing
+    {
+        //optional int y parameter
+        public int Add(int x, int y = 0)
+        {
+            int result = x + y;
+            return result;
+        }
+
+
+    }
+}
