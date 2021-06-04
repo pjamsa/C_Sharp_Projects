b1729b4819aa3886e6bba08aeb6e2eda113cc3d2
pjamsa <Paul_Jamsa@msn.com>
2021-06-04 15:50:02 -0700

    Initial Build

---
+++ ClassAssignment/ClassAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassAssignment
+{
+    //changed to static Class
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Thing myObj = new Thing();
+            Console.WriteLine("Please enter a whole number");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            myObj.Divide(userNum);
+
+            // declared variables but no attached values
+            int num1, num2;
+            
+            //passing multiple variables to method
+            Thing.Add(out num1, out num2);
+
+            Console.WriteLine("If you add this number by itself it is: {0}", num1);
+            Console.WriteLine("If you add this number by itself it is: {0}", num2);
+
+            // calling function and passing double parameter
+            myObj.Divide(100.5);
+            
+        }
+    }
+}
---
+++ ClassAssignment/ClassAssignment/Thing.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassAssignment
+{
+    class Thing
+    {
+        public void Divide(int x)
+        {
+            int result = x / 2;
+            Console.WriteLine(result);
+        }
+        // 1st overloading of function
+        public void Divide(double x)
+        {
+            double equation = x / 50.5;
+            int result = Convert.ToInt32(equation);
+            Console.WriteLine(result);
+        }
+
+        // passing in variables parameters output num1 and num2
+        public static void Add(out int num3, out int num4)
+        {
+            num3 = 100;
+            num4 = 200;
+            num3 += num3;
+            num4 += num4;
+        }
+        
+
+    }
+}
