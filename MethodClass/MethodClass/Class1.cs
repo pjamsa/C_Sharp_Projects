f29f0e7eceb427c2446fec1ebfcf5d190b558664
pjamsa <Paul_Jamsa@msn.com>
2021-06-04 11:33:57 -0700

    Initial Build

---
+++ MethodClass/MethodClass/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodClass
+{
+    class Class1
+    {
+        //created void method
+        public void Add(int x, int y)
+        {
+            int result = x + 365;
+            Console.WriteLine(result);
+            Console.WriteLine(y);
+        }
+    }
+}
---
+++ MethodClass/MethodClass/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instatiated class
+            Class1 myObj = new Class1();
+            // calling Add method and passing two numbers
+            myObj.Add(5, 6);
+        }
+    }
+}
