a419c12c0dc8af7d1066014d1d6e6155ecd4c126
pjamsa <Paul_Jamsa@msn.com>
2021-06-04 13:20:00 -0700

    Added Parameters by Name

--- MethodClass/MethodClass/Program.cs
+++ MethodClass/MethodClass/Program.cs
@@ -6,10 +6,11 @@ namespace MethodClass
     {
         static void Main(string[] args)
         {
-            // Instatiated class
+            // Instantiated class
             Class1 myObj = new Class1();
             // calling Add method and passing two numbers
             myObj.Add(5, 6);
+            myObj.Add(x: 13, y: 15);
         }
     }
 }
