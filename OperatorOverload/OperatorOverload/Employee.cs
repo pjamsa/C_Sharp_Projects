f11d7f205c4cef3a4dbb50e90303dbb5a6440bba
pjamsa <Paul_Jamsa@msn.com>
2021-06-07 10:50:11 -0700

    Initial Build

---
+++ OperatorOverload/OperatorOverload/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatorOverload
+{
+    class Employee
+    {
+        public int ID1;
+        public int ID2;
+
+        public static bool operator ==(ID1, ID2)
+        {
+            return true;
+        }
+    }
+}
---
+++ OperatorOverload/OperatorOverload/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OperatorOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            employee.ID1 = 3015;
+            employee.ID2 = 3016;
+
+            
+       
+        
+
+           
+        }
+    }
+}
