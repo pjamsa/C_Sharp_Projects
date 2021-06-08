79d834e99fcc261950b5dfbf7a95e8c604a2a782
pjamsa <Paul_Jamsa@msn.com>
2021-06-07 19:08:57 -0700

    Iniital Build

---
+++ EnumParse/EnumParse/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EnumParse
+{
+    class Program
+    {
+        public enum DayOfWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        static void Main(string[] args)
+        {
+            //asks user for input
+            Console.WriteLine("Please enter the current day of the week:");
+            string userDay = Console.ReadLine();
+            string value = userDay;
+            DayOfWeek day;
+            try
+            {
+                // if possible converts user input to enum 
+               day = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), value);
+                Console.WriteLine(userDay + " was recorded.  Thank you for your input.");
+            }
+            //gives user an error message if their input doesn't match
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                Console.WriteLine(ex.Message);             
+            }
+            
+            
+            
+        }
+    }
+}
