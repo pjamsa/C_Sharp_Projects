e7ee43940bdde8d245253230ff1844494e002c59
pjamsa <Paul_Jamsa@msn.com>
2021-06-07 19:52:51 -0700

    Initial Build

---
+++ Struct/Struct/Number.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Struct
+{
+    struct Number
+    {
+        public decimal Amount;
+    }
+}
---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Number number = new Number();
+            number.Amount = 300.5m;
+
+            Console.WriteLine(number.Amount);
+        }
+    }
+}
