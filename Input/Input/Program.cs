3bccfc4224ce28df0d4a3fd5a4ea19624b290f98
pjamsa <Paul_Jamsa@msn.com>
2021-06-09 13:24:41 -0700

    Initial Build

---
+++ Input/Input/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a whole number:");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C:\Users\paul_\Logs_c#\log2.txt", true))
+            {
+                file.WriteLine(userNum);
+            }
+            Console.WriteLine("Your information was saved in the file: log2.txt");
+
+        }
+    }
+}
