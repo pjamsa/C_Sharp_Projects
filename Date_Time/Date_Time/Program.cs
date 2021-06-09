490d27d729e99d1d3b56a5f7dd94197df67352cd
pjamsa <Paul_Jamsa@msn.com>
2021-06-09 15:42:15 -0700

    Initial Build

---
+++ Date_Time/Date_Time/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Date_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //displays current time
+            Console.WriteLine(DateTime.Now);
+            //get user input
+            Console.WriteLine("Please enter a number:");
+            // convert user input to an integer
+            int userNum = Convert.ToInt32(Console.ReadLine());
+            //add user input to current time
+            DateTime Later = DateTime.Now.AddHours(userNum);
+            Console.WriteLine("In {0} hours the time will be: " + Later, userNum);
+            
+            
+      
+        }
+    }
+}
