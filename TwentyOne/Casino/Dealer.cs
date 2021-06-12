a93122a55cdc361816aa1c27113863804b4b9ec5
pjamsa <Paul_Jamsa@msn.com>
2021-06-12 11:58:03 -0700

    Initial Build

---
+++ TwentyOne/Casino/Dealer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Casino
+{
+    class Dealer
+    {
+        public string Name { get; set; }
+        public Deck Deck { get; set; }
+        public int Balance { get; set; }
+
+        public void Deal(List<Card> Hand)
+        {
+            Hand.Add(Deck.Cards.First());
+            string card = string.Format(Deck.Cards.First().ToString() + "\n");
+            Console.WriteLine(card);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\paul_\Logs_c#\log.txt", true))
+            {
+                file.WriteLine(DateTime.Now);
+                file.WriteLine(card);
+            }
+            Deck.Cards.RemoveAt(0);
+        }
+    }
+}
---
+++ TwentyOne/Casino/IWalkAway.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Casino.Interfaces
+{
+    interface IWalkAway
+    {
+        public void WalkAway(Player player);
+
+    }
+}
