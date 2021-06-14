b34b9da6350411ca26cc245d75047523618325b1
pjamsa <Paul_Jamsa@msn.com>
2021-06-14 13:37:52 -0700

    Initial Build

---
+++ ChainConstructor/ChainConstructor/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChainConstructor
+{
+    // Chaining constructors fist constructors give a value to the first int ID;
+    public class Employee
+    {
+        public Employee() : this(9001)
+        {
+
+        }
+
+        public Employee(int ID)
+        {
+
+        }
+    }
+}
---
+++ ChainConstructor/ChainConstructor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ChainConstructor
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            //const variable has to be defined
+            const int a = 20;
+            // using var saves time and cuts down lengthy code
+            var b = 10;
+
+            Employee newEmployee = new Employee();
+
+            
+
+        }
+    }
+}
